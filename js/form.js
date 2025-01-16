f0a9d99c95c0fd85d91619db7168a3d5c07679be
liluppis <lisa.upp@hotmail.com>
2025-01-16 08:50:20 +0100

    updated styles

--- js/form.js
+++ js/form.js
@@ -1,5 +1,4 @@
-        // Example JavaScript for form validation or handling the form submission
-        document.getElementById("contact-form").addEventListener("submit", function(event) {
-            event.preventDefault();
-            alert("Formuläret har skickats!");
-        });
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("The form has been sent!!");
+});
