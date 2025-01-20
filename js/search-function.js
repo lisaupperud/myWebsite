5e5f0e38a9891aa753020baca357c22099263c3e
liluppis <lisa.upp@hotmail.com>
2025-01-20 09:16:52 +0100

    added srcset

--- js/search-function.js
+++ js/search-function.js
@@ -6,10 +6,10 @@ function filterSkills() {
     skillsList = document.getElementById("skillsList");
     skills = skillsList.getElementsByClassName("skill");
 
-    // Logga för att kolla på det som skrivs i sökfältet
+    // logga för att kolla på det som skrivs i sökfältet
     console.log("Searching for: " + filter);  
 
-    // Loop through all skills and hide those who don't match the search query
+    // loop through all skills and hide those who don't match the search query
     for (i = 0; i < skills.length; i++) {
         skill = skills[i];
         title = skill.getElementsByTagName("h2")[0];
